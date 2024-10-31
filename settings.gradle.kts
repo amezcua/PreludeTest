e9fee77954f922972d11652a3ba7b17065121805
Alejandro <alejandro.mezcua@128chars.com>
2024-10-31 17:11:02 +0100

    New release

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    repositories {
+        google()
+        mavenLocal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Prelude"
+include(":sdk")
+
+include(":core-sdk")
+include(":demo")
+project(":demo").projectDir = file("../../../sdk/demo/android/Demo/app")
