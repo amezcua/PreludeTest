548b3ed17fa86f716fd304fe0d61d62f04b5f98c
Alejandro <alejandro.mezcua@128chars.com>
2024-11-13 19:49:20 +0100

    New release

--- Package.swift
+++ Package.swift
@@ -21,8 +21,9 @@ let package = Package(
             dependencies: ["PreludeCore"]
         ),
         .binaryTarget(
-            name: "PreludeCore",
-            path: "./core/PreludeCore.xcframework.zip"
-        ),
+              name: "PreludeCore",
+              url: "http://to/",
+              checksum: "e8ebee6a8cc803807e6fd2e89ed464ae897a045d5caff82555a8555f79a88ecc"
+          ),
     ]
 )
