a48b06f5b21f7f28af32bd07c911dd717cfb1cfd
Alejandro Mezcua <alejandro.mezcua@gmail.com>
2024-11-18 18:17:26 +0100

    Update Package.swift
    Make static lib

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "Prelude",
-            type: .dynamic,
+            // type: .dynamic,
             targets: ["Prelude", "PreludeCore"]
         ),
     ],
