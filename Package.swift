9fb83ceb635080076cbfdeb53204004fb57f6e10
Alejandro Mezcua <alejandro.mezcua@gmail.com>
2024-11-22 10:53:44 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "Prelude",
-            type: .static,
+            type: .dynamic,
             targets: ["Prelude", "PreludeCore"]
         ),
     ],
