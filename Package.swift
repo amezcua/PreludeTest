a4a7ed18f9c22fc43cb1c3be696c67c98b131dde
Alejandro Mezcua <alejandro.mezcua@gmail.com>
2024-11-25 11:41:39 +0100

    Update Package.swift. Remote core to cloudflare

--- Package.swift
+++ Package.swift
@@ -21,7 +21,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "PreludeCore",
-            path: "./core/PreludeCore.xcframework.zip"
+            url: "https://pub-cac6f86c3ee04b919167390091469db5.r2.dev/PreludeCore-0.1.0.xcframework.zip",
+            checksum: "4a86e74d3e4ba57c721f441b24e2400469ab811b2bef75c2c0cf698e518947ad"
         ),
     ]
 )
