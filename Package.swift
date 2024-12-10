e16337ef4ba0dd64c634a41be263849d01f895a5
Alejandro <alejandro.mezcua@128chars.com>
2024-12-10 17:05:07 +0100

    SDK Update

--- Package.swift
+++ Package.swift
@@ -20,9 +20,9 @@ let package = Package(
             dependencies: ["PreludeCore"]
         ),
         .binaryTarget(
-              name: "PreludeCore",
-              url: "https://prelude-public.s3.amazonaws.com/sdk/releases/apple/core/0.1.0/PreludeCore-0.1.0.xcframework.zip",
-              checksum: "8787dcbef29121e03f98bd80e53ba2529b167c456b1d5c2beaef77241ca50bc2"
-          ),
+            name: "PreludeCore",
+            url: "https://prelude-public.s3.amazonaws.com/sdk/releases/apple/core/0.1.0/PreludeCore-0.1.0.xcframework.zip",
+            checksum: "d9c09b7872ca3625bf11c468d8b49d3ad18153a6c4f16cdf0a380dd001993ddc"
+        ),
     ]
 )
